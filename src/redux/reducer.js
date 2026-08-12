f8dd5ed5d046063dd2c7a579e9d9611f47fdab37
kayalvizhi <madhukayal88@gmail.com>
2026-08-12 11:08:34 +0530

    Implement books reducer with initial state and actions

---
+++ src/redux/reducer.js
@@ -0,0 +1,32 @@
+import {
+  FETCH_BOOKS_REQUEST,
+  FETCH_BOOKS_SUCCESS,
+  FETCH_BOOKS_FAILURE,
+  SET_SORT_BY,
+  SET_SORT_ORDER,
+} from './actions';
+
+const initialState = {
+  books: [],
+  loading: false,
+  error: null,
+  sortBy: 'title', // Default sort criteria
+  sortOrder: 'asc', // Default sort order
+};
+
+export const booksReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_BOOKS_REQUEST:
+      return { ...state, loading: true, error: null };
+    case FETCH_BOOKS_SUCCESS:
+      return { ...state, loading: false, books: action.payload };
+    case FETCH_BOOKS_FAILURE:
+      return { ...state, loading: false, error: action.payload };
+    case SET_SORT_BY:
+      return { ...state, sortBy: action.payload };
+    case SET_SORT_ORDER:
+      return { ...state, sortOrder: action.payload };
+    default:
+      return state;
+  }
+};
