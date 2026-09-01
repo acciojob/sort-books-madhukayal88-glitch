c0c678bbdd8ecd955036d74d80e7f719c2d5c2d5
kayalvizhi <madhukayal88@gmail.com>
2026-09-01 17:13:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 import BooksList from './components/BooksList';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <BooksList />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>Book Sorting App</h1>
+        <BooksList />
+      </div>
+    </Provider>
   );
 }
 
