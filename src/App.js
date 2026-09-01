7eb4095b9ae29883a7d616ef3b044c8560740665
kayalvizhi <madhukayal88@gmail.com>
2026-09-01 17:19:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import store from './store/store';
 import BooksList from './components/BooksList';
+import './App.css';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <h1>Book Sorting App</h1>
         <BooksList />
       </div>
     </Provider>
